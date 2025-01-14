3813d260cfe47083c08a4cb8e3cd197b439c094e
IDGS-801-22002332 <jesusisaguirr@gmail.com>
2025-01-13 22:36:06 -0600

    Primer commit

---
+++ 01-variables.py
@@ -0,0 +1,14 @@
+print('Ejemplo1: ')
+
+num1=5
+num2=4
+rest= num1+num2
+print(rest)
+
+print('Dame dos numeros para sumarlos')
+print('')
+num1=int(input('Ingresa el primer numero: '))
+num2=input('Ingresa el segundo numero: ')
+suma=num1+int(num2)
+
+print('la suma es  {1} mas {2} es:{0}'.format(suma,num1,num2))
